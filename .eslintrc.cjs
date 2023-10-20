0d267de618bb6347d4e140661de47e733780a15b
Sérgio Félix <sergio98@outlook.pt>
2023-10-20 17:29:08 +0100

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: true,
+	},
+	plugins: ['@typescript-eslint', '@tanstack/query'],
+	extends: [
+		'next/core-web-vitals',
+		'plugin:@typescript-eslint/recommended-type-checked',
+		'plugin:@typescript-eslint/stylistic-type-checked',
+		'plugin:@tanstack/eslint-plugin-query/recommended',
+	],
+	rules: {
+		'@typescript-eslint/array-type': 'off',
+		'@typescript-eslint/consistent-type-definitions': 'off',
+
+		'@typescript-eslint/consistent-type-imports': [
+			'warn',
+			{
+				prefer: 'type-imports',
+				fixStyle: 'inline-type-imports',
+			},
+		],
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{ argsIgnorePattern: '^_' },
+		],
+		'@typescript-eslint/no-misused-promises': [
+			2,
+			{
+				checksVoidReturn: { attributes: false },
+			},
+		],
+		'@tanstack/query/exhaustive-deps': 'error',
+		'@tanstack/query/stable-query-client': 'error',
+	},
+};
+
+module.exports = config;
