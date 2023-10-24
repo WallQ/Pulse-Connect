12d2099bd49a3004110f343743c3653556b671bf
Sérgio Félix <sergio98@outlook.pt>
2023-10-25 00:44:24 +0100

    Redirect if not authenticated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import Link from 'next/link';
 
 import { ROUTES } from '@/constants/routes';
+import { getServerAuthSession } from '@/server/auth';
+import { redirect } from 'next/navigation';
 
-export default function HomePage() {
+export default async function HomePage() {
+	const session = await getServerAuthSession();
+
+	if (!session) {
+		redirect(ROUTES.AUTH.SIGNIN);
+	}
+
 	return (
 		<div>
 			<h1>Hello World!</h1>
 			<Link href={ROUTES.AUTH.SIGNIN}>
 				<span className='text-base text-black'>Sign In</span>
+			</Link>
+			<Link href={ROUTES.AUTH.SIGNUP}>
+				<span className='text-base text-black'>Sign Up</span>
 			</Link>
 		</div>
 	);
