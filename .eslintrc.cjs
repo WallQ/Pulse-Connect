d2031b7e333d399e8e9165279265ec1142ed6389
Sérgio Félix <sergio98@outlook.pt>
2023-10-24 01:40:14 +0100

    Added sort imports and TanStack plugins

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ const config = {
 	parserOptions: {
 		project: true,
 	},
-	plugins: ['@typescript-eslint', '@tanstack/query'],
+	plugins: ['@typescript-eslint', 'simple-import-sort', '@tanstack/query'],
 	extends: [
 		'next/core-web-vitals',
 		'plugin:@typescript-eslint/recommended-type-checked',
@@ -14,7 +14,6 @@ const config = {
 	rules: {
 		'@typescript-eslint/array-type': 'off',
 		'@typescript-eslint/consistent-type-definitions': 'off',
-
 		'@typescript-eslint/consistent-type-imports': [
 			'warn',
 			{
@@ -32,6 +31,8 @@ const config = {
 				checksVoidReturn: { attributes: false },
 			},
 		],
+		'simple-import-sort/imports': 'error',
+		'simple-import-sort/exports': 'error',
 		'@tanstack/query/exhaustive-deps': 'error',
 		'@tanstack/query/stable-query-client': 'error',
 	},
