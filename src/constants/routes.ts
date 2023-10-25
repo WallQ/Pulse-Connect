12464b4cb3ddb8b16cabf1f7c6d78b7a519b55b9
Sérgio Félix <sergio98@outlook.pt>
2023-10-25 23:51:40 +0100

    New routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,9 +1,14 @@
 export const ROUTES = {
-	ROOT: '/',
+	HOME: '/',
+	CONNECTIONS: '/connections',
+	MESSAGES: '/messages',
+	NOTIFICATIONS: '/notifications',
+	PROFILE: '/profile',
+	SETTINGS: '/settings',
 	AUTH: {
-		BASE: '/auth',
 		SIGNIN: '/auth/signin',
 		SIGNUP: '/auth/signup',
+		SIGNOUT: '/auth/signout',
 		FORGOT_PASSWORD: '/forgot-password',
 		RESET_PASSWORD: '/reset-password',
 	},
