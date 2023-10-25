4cbd9d17d50f715c8ae4581f4c165eb7713ca286
Sérgio Félix <sergio98@outlook.pt>
2023-10-26 00:26:18 +0100

    Opengraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,8 +57,38 @@ export const metadata: Metadata = {
 		},
 	],
 	manifest: '/site.webmanifest',
-	openGraph: {},
-	twitter: {},
+	openGraph: {
+		title: 'Pulse Connect',
+		description:
+			'This is an evaluation project of the LDS curricular unit.',
+		url: 'https://pulse-connect-omega.vercel.app/',
+		siteName: 'Pulse Connect',
+		images: [
+			{
+				url: 'https://pulse-connect-omega.vercel.app/logo/Logo Transparent.svg',
+				width: 1200,
+				height: 630,
+				alt: 'Pulse Connect',
+			},
+		],
+		locale: 'en-US',
+		type: 'website',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Pulse Connect',
+		description:
+			'This is an evaluation project of the LDS curricular unit.',
+		creator: '@pulseconnect',
+		images: [
+			{
+				url: 'https://pulse-connect-omega.vercel.app/logo/Logo Transparent.svg',
+				width: 1200,
+				height: 630,
+				alt: 'Pulse Connect',
+			},
+		],
+	},
 	appleWebApp: {},
 	other: {
 		'apple-mobile-web-app-title': 'Pulse Connect',
